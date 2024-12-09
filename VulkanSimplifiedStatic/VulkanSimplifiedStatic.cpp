d7ba88099137b6c7aad702c270e58e8b9aaea737
JJBcku <154745800+JJBcku@users.noreply.github.com>
2024-12-09 03:28:52 +0100

    First commit

---
+++ VulkanSimplifiedStatic/VulkanSimplifiedStatic.cpp
@@ -0,0 +1,10 @@
+// VulkanSimplifiedStatic.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnVulkanSimplifiedStatic()
+{
+}
